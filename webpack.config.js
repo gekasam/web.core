49fc6a2781ad6a99af604d386c9a171b07a333ae
gekasam <gekasam94@gmail.com>
2023-11-16 01:21:09 +0300

    6.1.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,56 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  devServer: {
-    watchFiles: path.join(__dirname, 'src'),
-    port: 3200
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'static/images/[name][ext]'
-        }
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|)$/,
-        type: 'asset/inline',
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              '@babel/preset-env'
-            ]
+module.exports = (env) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+    devServer: {
+      watchFiles: path.join(__dirname, 'src'),
+      port: 3200
+    },
+    devtool: env.prod ? '' : 'source-map',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/images/[name][ext]'
           }
-        }
-      },
-    ]
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf|)$/,
+          type: 'asset/inline',
+        },
+        {
+          test: /\.(scss|css)$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+          
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          }
+        },
+      ]
+    }
   }
 };
